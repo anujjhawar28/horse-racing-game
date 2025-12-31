9d9d65c964bd8f0b4b4b51f781b88ff8de193fb5
Anuj Jhawar <anujjhawar28@gmail.com>
2025-12-31 23:22:47 +0530

    Refactor the code

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
-/**
- * Horse Racing Game - Application Entry Point
- * Initializes Vue application with Vuex store
- */
 import { createApp } from 'vue';
 import { store, key } from './store';
 import './style.css';
 import App from './App.vue';
 
-// Create and mount the application
 const app = createApp(App);
-
-// Use Vuex store
 app.use(store, key);
-
-// Mount to DOM
 app.mount('#app');
